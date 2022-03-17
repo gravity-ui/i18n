ef05ed3ad5926593198432f96fa06fc9ae3d683b
Alexey <sudilovskiy@yandex-team.ru>
2022-03-17 12:03:10 +0300

    chore: add tests (#12)
    * fix `package.json` deps order
    * setup `jest`
    * add tests for `#i18n` and `#has`
    * exclude tests from TS compilation

---
+++ src/index.spec.ts
@@ -0,0 +1,132 @@
+import {I18N} from "./index";
+
+let i18n: I18N;
+
+beforeEach(() => {
+    i18n = new I18N();
+});
+
+describe('has', () => {
+    it('should return false w/out any data available', () => {
+        expect(i18n.has('notification', 'title')).toBe(false);
+    });
+
+    it('should return false when keyset is missing', () => {
+        i18n.setLang('ru');
+        i18n.registerKeyset('ru', 'notification', {
+            title: 'New version'
+        });
+
+        expect(i18n.has('button', 'label')).toBe(false);
+    });
+
+    it('should return false when key is missing', () => {
+        i18n.setLang('ru');
+        i18n.registerKeyset('ru', 'notification', {
+            title: 'New version'
+        });
+
+        expect(i18n.has('notification', 'label')).toBe(false);
+    });
+
+    it('should return true when key exist', () => {
+        i18n.setLang('ru');
+        i18n.registerKeyset('ru', 'notification', {
+            title: 'New version'
+        });
+
+        expect(i18n.has('notification', 'title')).toBe(true);
+    });
+});
+
+describe('i18n', () => {
+    it('should return key when translation missing', () => {
+        i18n.setLang('ru');
+        expect(i18n.i18n('notification', 'title')).toBe('title');
+    });
+
+    it('should return key when keyset missing', () => {
+        i18n.setLang('ru');
+        i18n.registerKeyset('ru', 'notification', {
+            title: 'New version'
+        });
+        expect(i18n.i18n('button', 'title')).toBe('title');
+    });
+
+    it('should return key when keyset is empty', () => {
+        i18n.setLang('ru');
+        i18n.registerKeyset('ru', 'notification', {});
+        expect(i18n.i18n('notification', 'title')).toBe('title');
+    });
+
+    it('should return key when key missing', () => {
+        i18n.setLang('ru');
+        i18n.registerKeyset('ru', 'notification', {
+            title: 'New version'
+        });
+        expect(i18n.i18n('notification', 'label')).toBe('label');
+    });
+
+    it('should return translation', () => {
+        i18n.setLang('ru');
+        i18n.registerKeyset('ru', 'notification', {
+            title: 'New version'
+        });
+        expect(i18n.i18n('notification', 'title')).toBe('New version');
+    });
+
+    it('should return key when plural translation missing "count" param', () => {
+        i18n.setLang('ru');
+        i18n.registerKeyset('ru', 'notification', {
+            title: ['New version', 'New versions', 'New versions']
+        });
+        expect(i18n.i18n('notification', 'title', {})).toBe('title');
+    });
+
+    it('should return correct pluralization', () => {
+        i18n.setLang('ru');
+        i18n.registerKeyset('ru', 'notification', {
+            title: ['New version of {{project}}', 'New versions of {{project}}', 'New versions of {{project}}', 'Is up to date']
+        });
+        const project = 'Cloud';
+
+        expect(i18n.i18n('notification', 'title', {
+            count: 1,
+            project
+        })).toBe('New version of Cloud');
+        expect(i18n.i18n('notification', 'title', {
+            count: 2,
+            project
+        })).toBe('New versions of Cloud');
+        expect(i18n.i18n('notification', 'title', {
+            count: 10,
+            project
+        })).toBe('New versions of Cloud');
+        expect(i18n.i18n('notification', 'title', {
+            count: 0,
+            project
+        })).toBe('Is up to date');
+    });
+
+    it('should interpolate params', () => {
+        i18n.setLang('ru');
+        i18n.registerKeyset('ru', 'notification', {
+            title: 'Hello, {{username}}!'
+        });
+
+        expect(i18n.i18n('notification', 'title', {
+            username: 'Joe'
+        })).toBe('Hello, Joe!');
+    });
+
+    it('should accept dollar-sign in params', () => {
+        i18n.setLang('ru');
+        i18n.registerKeyset('ru', 'notification', {
+            title: 'Give me {{money}}!'
+        });
+
+        expect(i18n.i18n('notification', 'title', {
+            money: 'money $ honey'
+        })).toBe('Give me money $ honey!');
+    });
+});
