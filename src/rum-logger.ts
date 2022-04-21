32ea385fe45d07d8114d10a140a128de1d807291
Alexey <sudilovskiy@yandex-team.ru>
2022-04-21 17:43:33 +0300

    chore: reduce deprecation spam (#24)

--- src/rum-logger.ts
+++ src/rum-logger.ts
@@ -24,16 +24,16 @@ const warnCache = new Set();
  */
 export const rumLogger: Logger = {
     log(message, {level, logger, extra} = {}) {
-        if (typeof __webpack_require__ !== 'undefined' && process.env.NODE_ENV === 'development') {
-            console.log('@yandex-cloud/i18n: default logger is deprecated, and would be removed in future. Consult docs for alternative.');
-        }
-
         if (typeof window === 'undefined' || typeof window.Ya?.Rum?.logError !== 'function') {
             return;
         }
 
         if (warnCache.has(logger)) {
             return;
+        }
+
+        if (typeof __webpack_require__ !== 'undefined' && process.env.NODE_ENV === 'development') {
+            console.warn('@yandex-cloud/i18n: default logger is deprecated, and would be removed in future. Consult docs for alternative.');
         }
 
         console.warn(`[${extra?.type}][${logger || ''}] ${message}`);
